717d8f4181f92310c1056d2d80fa16fc02833bff
crystaltys <51997682+crystaltys@users.noreply.github.com>
2020-09-28 15:36:34 +0800

    Update steganographyGUI.py

--- acw_part2/steganographyGUI.py
+++ acw_part2/steganographyGUI.py
@@ -3,74 +3,100 @@ import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter import StringVar
 from tkinter import messagebox
-from tkinter import Canvas
+from tkinter import Canvas, Scrollbar
 from tkinter import filedialog
 from PIL import ImageTk,Image  
 
-global img
-
 class GUI():
-    def __init__(self, window):
 
-        # basic windows frame 
+    def __init__(self, window):
+        
+        # parent window 
         self.window = window
-        self.window.title("SteganographyGUI")
-        self.tabControl = ttk.Notebook(self.window) 
+        self.window.title("QuickStego")
         self.window.geometry("810x450")
         self.window.resizable(0, 0)
         
-        # top bar tab
-        self.encodeTab = ttk.Frame(self.tabControl) 
-        self.decodeTab = ttk.Frame(self.tabControl) 
-        self.tabControl.add(self.encodeTab, text ="Encoding") 
-        self.tabControl.add(self.decodeTab, text ="Decoding") 
-        self.tabControl.grid(row=0,column=0,pady=2,padx=2,columnspan=3,rowspan=7,sticky=tk.W)
-
+        # child window
+        self.child = ttk.Notebook(self.window) 
+        
+        # menu bar
+        self.encodeTab = ttk.Frame(self.child) 
+        self.decodeTab = ttk.Frame(self.child) 
+        self.child.add(self.encodeTab, text ="Encoding") 
+        self.child.add(self.decodeTab, text ="Decoding") 
+        self.child.grid(row=0,column=0,pady=2,padx=2,columnspan=7,rowspan=7,sticky=tk.W)
+        
+        # widgets
+        self.titleBar(self.encodeTab,self.decodeTab) 
+        self.btn(self.encodeTab,self.decodeTab) 
+        self.entryBox(self.encodeTab,self.decodeTab)
+        
+        # img widgets 
+        self.inputImage = tk.Canvas(self.encodeTab,width=260,height=300,background="white")
+        self.inputImage.grid(row=2,column=0,columnspan=2,rowspan=3,padx=2,sticky=tk.NW)
+        self.outputImage = tk.Canvas(self.encodeTab,width=260,height=300,background="white")
+        self.outputImage.grid(row=2,column=5,columnspan=2,rowspan=3,padx=2,sticky=tk.NW)   
+    
+    def titleBar(self,encode,decode):
         # left pane 
-        self.orgimgLabel = ttk.Label(self.encodeTab,text="Original Image")
+        self.orgimgLabel = ttk.Label(encode,text="Cover Image")
         self.orgimgLabel.grid(row=1,column=0,pady=5,padx=2,sticky=tk.SW)
-        self.inputImage = Canvas(self.encodeTab,width=260,height=260,background="white")
-        self.inputImage.grid(row=2,column=0,columnspan=2,rowspan=5,padx=2,sticky=tk.NW)
-        self.uploadButton=ttk.Button(self.encodeTab,text="Browse",padding=2,command=self.load_coverimg)
-        self.uploadButton.grid(row=4,column=0,padx=2,ipady=4,ipadx=25,sticky=tk.NW)
-        self.resetButton=ttk.Button(self.encodeTab,text="Clear",padding=2)
-        self.resetButton.grid(row=4,column=1,padx=2,ipady=4,ipadx=25,sticky=tk.NW)
-
         # middle pane
-        self.keyLabel = ttk.Label(self.encodeTab,text="Encryption Key")
-        self.keyLabel.grid(row=1,column=2,columnspan=3,pady=5,padx=10,sticky=tk.W)
-        self.txtEncryptionKey=StringVar()
-        self.entryKey=ttk.Entry(self.encodeTab,textvariable=self.txtEncryptionKey,width=40)
-        self.entryKey.grid(row=2,column=2,columnspan=3,padx=10,pady=2,ipady=120,sticky=tk.NW)   
-        self.secretmsgLabel = ttk.Label(self.encodeTab,text="Secret Message")
-        self.secretmsgLabel.grid(row=3,column=2,padx=10,pady=2,sticky=tk.NW)
-        self.txtSecretMsg=StringVar()
-        self.secretMsg=ttk.Entry(self.encodeTab,textvariable=self.txtSecretMsg,width=40)
-        self.secretMsg.grid(row=4,column=2,columnspan=3,padx=10,ipady=5,sticky=tk.NW)
-        self.thresholdLabel = ttk.Label(self.encodeTab,text="Threshold")
-        self.thresholdLabel.grid(row=5,column=2,padx=10,sticky=tk.NW)
-        self.txtThreshold=StringVar()
-        self.threshold=ttk.Entry(self.encodeTab,textvariable=self.txtThreshold,width=40)
-        self.threshold.grid(row=6,column=2,columnspan=3,padx=10,sticky=tk.NW)
-
+        self.secretMsg = ttk.Label(encode,text="Secret Payload")
+        self.secretMsg.grid(row=1,column=2,columnspan=3,pady=5,padx=10,sticky=tk.W)
+        self.secretKey = ttk.Label(encode,text="Secret Key")
+        self.secretKey.grid(row=4,column=2,columnspan=1,pady=5,padx=4,sticky=tk.W)
+        self.threshold = ttk.Label(encode,text="Threshold")
+        self.threshold.grid(row=5,column=2,columnspan=1,pady=5,padx=4,sticky=tk.W)
         # right pane
-        self.stegoimgLabel = ttk.Label(self.encodeTab,text="Stego Image")
-        self.stegoimgLabel.grid(row=1,column=5,pady=5,padx=2,sticky=tk.SW)
-        self.outputImage = tk.Canvas(self.encodeTab,width=260,height=260,background="white")
-        self.outputImage.grid(row=2,column=5,columnspan=2,rowspan=2,padx=2,sticky=tk.NW)
+        self.stegoimgLabel = ttk.Label(encode,text="Stego Image")
+        self.stegoimgLabel.grid(row=1,column=5,pady=5,sticky=tk.W)
 
-        self.createButton=ttk.Button(self.encodeTab,text="Create Stego Image",padding=2,command=self.load_coverimg)
-        self.createButton.grid(row=4,column=5,columnspan=2,padx=2,ipady=4,ipadx=75,sticky=tk.NW)
+    def btn(self,encode,decode):  
+        # left pane   
+        self.uploadButton=ttk.Button(encode,text="Browse",padding=2,command=self.load_coverImg)
+        self.uploadButton.grid(row=5,column=0,padx=2,pady=5,ipady=4,ipadx=26,sticky=tk.NW)
+        self.resetButton=ttk.Button(encode,text="Clear",padding=2,command=self.clear_coverImg)
+        self.resetButton.grid(row=5,column=1,padx=2,pady=5,ipady=4,ipadx=26,sticky=tk.NW)
+        self.createButton=ttk.Button(encode,text="Start Encoding",padding=2)
+        self.createButton.grid(row=5,column=5,columnspan=2,padx=2,pady=4,ipady=4,ipadx=85,sticky=tk.NW)
+
+    def entryBox(self,encode,decode):  
+        # middle pane
+        self.secretMsg_txtBox = tk.Text(encode, height=16, width=30)
+        self.secretMsg_txtBox.grid(row=2,column=2,columnspan=3,rowspan=2,padx=8,sticky=tk.W) 
+        self.secretMsg_txtBox.bind("<Key>", self.load_secretPayload)
+        self.txtsecretKey=StringVar()
+        self.entryKey=ttk.Entry(encode,textvariable=self.txtsecretKey,width=28)
+        self.entryKey.grid(row=4,column=3,pady=5,ipady=4,sticky=tk.NSEW)   
+        
+        self.txtThreshold=StringVar()
+        self.txtThreshold.set("0.3")
+        self.entryThreshold=ttk.Entry(encode,textvariable=self.txtThreshold,width=28)
+        self.entryThreshold.grid(row=5,column=3,pady=5,ipady=5,sticky=tk.NSEW)   
+
+    # event functions
     
-    def load_coverimg(self):
-        self.filename = filedialog.askopenfilename(initialdir="./", title="Select An Image", filetype =(("png","*.png"), ("All Files","*.*")))
-        self.inputImage.create_image(120, 60,image=img) 
+    def load_coverImg(self):
+        self.filename = filedialog.askopenfilename(initialdir="./img", title="Open File", filetype =(("*.png","*.png"),("*.jpg","*.jpg"),("*.bmp","*.bmp"),("All Files","*.*")))
+        # create PIL image
+        self.img = Image.open(self.filename)
+        self.img.thumbnail((300, 400), Image.ANTIALIAS)
+        #create photoimage
+        self.image = ImageTk.PhotoImage(self.img)
+        self.inputImage.create_image(0,0,image=self.image, anchor=tk.NW)
+       
+    def clear_coverImg(self):
+        self.inputImage.delete("all")
     
-    #def encrypt(self):
+    def load_secretPayload(self,event):
+        self.input = self.secretMsg_txtBox.get("1.0","end-1c")
+        print(self.input)
+        
 
 
 if __name__ == "__main__":
     window = tk.Tk()
-    img = ImageTk.PhotoImage(Image.open("test.png"))  
     GUI(window)
     window.mainloop()
